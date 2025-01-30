22eb50201a5c4899a74c11b4a8bfa489316d0004
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2025-01-30 20:16:59 +0200

    Fix jdbc url building.

--- mysql/src/main/java/me/flame/universal/mysql/connections/SimpleConnectionProvider.java
+++ mysql/src/main/java/me/flame/universal/mysql/connections/SimpleConnectionProvider.java
@@ -2,6 +2,7 @@ package me.flame.universal.mysql.connections;
 
 import me.flame.universal.api.connection.ConnectionProvider;
 import me.flame.universal.mysql.credentials.MySQLCredentials;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,36 +10,32 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public class SimpleConnectionProvider implements ConnectionProvider<Connection> {
-    private Connection connection;
+    public static final String BASE_URL = "jdbc:mysql://%s:%s/%s";
+    private final String url;
 
-    public SimpleConnectionProvider(final MySQLCredentials credentials) {
-        try {
-            StringBuilder url = new StringBuilder(credentials.jdbcUrl());
-            boolean isFirst = true;
+    public SimpleConnectionProvider(final @NotNull MySQLCredentials credentials) {
+        StringBuilder url = new StringBuilder(String.format(BASE_URL, credentials.host(), credentials.port(), credentials.database()));
 
-            for (Map.Entry<String, String> entry : credentials.dataSourceProperties().entrySet()) {
-                if (isFirst) {
-                    url.append('?');
-                    isFirst = false;
-                } else {
-                    url.append('&');
-                }
-                url.append(entry.getKey()).append('=').append(entry.getValue());
+        boolean first = true;
+        for (Map.Entry<String, String> entry : credentials.dataSourceProperties().entrySet()) {
+            if (first) {
+                url.append('?');
+                first = false;
+            } else {
+                url.append('&');
             }
-            this.connection = DriverManager.getConnection(url.toString());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            url.append(entry.getKey()).append('=').append(entry.getValue());
         }
+        this.url = url.toString();
 
     }
 
     @Override
-    public Connection getConnection() {
-        return connection;
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url);
     }
 
     @Override
     public void close() {
-        this.connection = null;
     }
 }
