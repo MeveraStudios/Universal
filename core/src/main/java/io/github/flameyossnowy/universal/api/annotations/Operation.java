bc1101ee25c78f65cdb9ffe92e5da107b5688a19
FlameyosSnowy <eyadamrfawzy7@duck.com>
2025-02-08 14:54:09 +0200

    Add index and casting.

---
+++ core/src/main/java/io/github/flameyossnowy/universal/api/annotations/Operation.java
@@ -0,0 +1,16 @@
+package io.github.flameyossnowy.universal.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Operation {
+    String field() default "";
+
+    String op() default "";
+
+    String value() default "";
+}
