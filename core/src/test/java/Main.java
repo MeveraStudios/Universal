d14ed541bde9d2b5f99d0a2e58c0004988f0bc1f
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-03-25 23:30:41 +0200

    Useless test file

--- core/src/test/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-    }
-}
