79b9801c6e03bff187b046d6e168e32358681164
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-05-10 12:33:48 +0300

    Add postgresql and SQLObjectFactory

---
+++ postgresql/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.flameyossnowy.universal"
+version = "4.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    // hikaricp
+    implementation("com.zaxxer:HikariCP:6.2.1")
+
+    compileOnly(project(":core"))
+    compileOnly(project(":sql-common"))
+
+    compileOnly("org.jetbrains:annotations:24.0.1")
+
+    // postgresql
+    compileOnly("org.postgresql:postgresql:42.7.2")
+
+    testImplementation("org.postgresql:postgresql:42.7.2")
+    testImplementation(project(":core"))
+    testImplementation(project(":sql-common"))
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ sql-common/src/main/java/io/github/flameyossnowy/universal/sql/internals/SQLObjectFactory.java
@@ -0,0 +1,34 @@
+package io.github.flameyossnowy.universal.sql.internals;
+
+import io.github.flameyossnowy.universal.api.factory.DatabaseObjectFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Interface for SQL object factories.
+ * @param <T> the element class
+ * @param <ID> the id class
+ */
+public interface SQLObjectFactory<T, ID> extends DatabaseObjectFactory<T, ID, ResultSet> {
+    /**
+     * Inserts a single entity into the database using the given PreparedStatement.
+     *
+     * @param statement The prepared statement to be used to insert the entity.
+     * @param entity    The entity to be inserted.
+     * @throws Exception If an error occurs.
+     */
+    void insertEntity(PreparedStatement statement, T entity) throws Exception;
+
+    /**
+     * Inserts all collection fields of the given entity using the given PreparedStatement.
+     * This method is called after the primary entity has been inserted and the ID of the primary
+     * entity has been set.
+     *
+     * @param entity    The entity to be inserted.
+     * @param id        The ID of the primary entity.
+     * @param statement The prepared statement to be used to insert the collections.
+     * @throws Exception If an error occurs.
+     */
+    void insertCollectionEntities(T entity, ID id, PreparedStatement statement) throws Exception;
+}
