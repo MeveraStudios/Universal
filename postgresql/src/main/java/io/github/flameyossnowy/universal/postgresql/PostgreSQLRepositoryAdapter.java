e19519bc9a42f84638579cdc2252909be9c4393d
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-05-10 21:09:24 +0300

    Fix an issue which made other existing databases irrelevant. (Ruining universal principle)

--- postgresql/src/main/java/io/github/flameyossnowy/universal/postgresql/PostgreSQLRepositoryAdapter.java
+++ postgresql/src/main/java/io/github/flameyossnowy/universal/postgresql/PostgreSQLRepositoryAdapter.java
@@ -6,21 +6,13 @@ import io.github.flameyossnowy.universal.sql.internals.AbstractRelationalReposit
 import io.github.flameyossnowy.universal.sql.internals.QueryParseEngine;
 import io.github.flameyossnowy.universal.sql.internals.ResultCache;
 import io.github.flameyossnowy.universal.sql.internals.SQLConnectionProvider;
-import io.github.flameyossnowy.universal.sql.resolvers.ValueTypeResolverRegistry;
+
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.net.InetAddress;
-import java.util.UUID;
 import java.util.function.LongFunction;
 
 public class PostgreSQLRepositoryAdapter<T, ID> extends AbstractRelationalRepositoryAdapter<T, ID> {
-    static {
-        ValueTypeResolverRegistry.INSTANCE.registerEncodedTypeMapper(InetAddress.class, "INET");
-        ValueTypeResolverRegistry.INSTANCE.registerEncodedTypeMapper(UUID.class, "UUID");
-        ValueTypeResolverRegistry.INSTANCE.registerEncodedTypeMapper(String.class, "TEXT");
-    }
-
     protected PostgreSQLRepositoryAdapter(@NotNull final SQLConnectionProvider dataSource, final ResultCache<T, ID> cache, final Class<T> repository, final Class<ID> idClass, SessionCache<ID, T> globalCache, LongFunction<SessionCache<ID, T>> sessionCacheLongFunction) {
         super(dataSource, cache, repository, idClass, QueryParseEngine.SQLType.POSTGRESQL, globalCache, sessionCacheLongFunction);
         this.setObjectFactory(new PostgresObjectFactory<>(repositoryInformation, dataSource, this));
