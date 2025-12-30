ede6fc4fe6569161f4cb6badc47def2996138924
flameyosflow <flamesfflowsnow@gmail.com>
2025-12-30 12:05:59 +0200

    New functions meant for internal usage.

--- core/src/main/java/io/github/flameyossnowy/universal/api/handler/RelationshipHandler.java
+++ core/src/main/java/io/github/flameyossnowy/universal/api/handler/RelationshipHandler.java
@@ -3,15 +3,22 @@ package io.github.flameyossnowy.universal.api.handler;
 import io.github.flameyossnowy.universal.api.reflect.FieldData;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Generic, portable relationship handler API.
  * Defines abstract relationship accessors that any backend can implement.
  */
-public interface RelationshipHandler<ID> {
+public interface RelationshipHandler<T, ID> {
     Object handleManyToOneRelationship(ID primaryKeyValue, FieldData<?> field);
 
     Object handleOneToOneRelationship(ID primaryKeyValue, FieldData<?> field);
 
     List<Object> handleOneToManyRelationship(FieldData<?> field, ID primaryKeyValue);
+
+    void prefetch(Iterable<?> results, Set<String> prefetch);
+
+    void invalidateRelationshipsForId(ID id);
+
+    void clear();
 }
