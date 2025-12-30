140e5b14ae6940c0d637ce26cc0cad21cd1f39b7
flameyosflow <flamesfflowsnow@gmail.com>
2025-12-30 12:07:02 +0200

    Readability

--- sql-common/src/main/java/io/github/flameyossnowy/universal/sql/internals/NoArrayObjectFactory.java
+++ sql-common/src/main/java/io/github/flameyossnowy/universal/sql/internals/NoArrayObjectFactory.java
@@ -1,21 +1,27 @@
 package io.github.flameyossnowy.universal.sql.internals;
 
 import io.github.flameyossnowy.universal.api.RelationalRepositoryAdapter;
+import io.github.flameyossnowy.universal.api.RepositoryAdapter;
 import io.github.flameyossnowy.universal.api.params.DatabaseParameters;
 import io.github.flameyossnowy.universal.api.reflect.FieldData;
 import io.github.flameyossnowy.universal.api.reflect.RepositoryInformation;
 import io.github.flameyossnowy.universal.api.resolver.TypeResolverRegistry;
 import io.github.flameyossnowy.universal.sql.DatabaseImplementation;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.Collection;
 
 public final class NoArrayObjectFactory<T, ID>
         extends ObjectFactory<T, ID> {
 
-    public NoArrayObjectFactory(RepositoryInformation repoInfo, SQLConnectionProvider connectionProvider, DatabaseImplementation implementation, RelationalRepositoryAdapter<T, ID> adapter, TypeResolverRegistry typeResolverRegistry) {
+    public NoArrayObjectFactory(
+        RepositoryInformation repoInfo,
+        SQLConnectionProvider connectionProvider,
+        DatabaseImplementation implementation,
+        RepositoryAdapter<T, ID, Connection> adapter,
+        TypeResolverRegistry typeResolverRegistry
+    ) {
         super(repoInfo, connectionProvider, implementation, adapter, typeResolverRegistry);
     }
 
